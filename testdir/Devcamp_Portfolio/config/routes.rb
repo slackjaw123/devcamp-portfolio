b3a07c4c75faabc413e9a1bc91da53581bcdd224
SD Davis <davis@devcamp.com>
2017-07-05 14:38:53 +0000

    new changes

--- testdir/Devcamp_Portfolio/config/routes.rb
+++ testdir/Devcamp_Portfolio/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#angular', as: 'portfolio_show'
+  get 'angular-items', to: 'portfolio#show'
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: 'pages#home'
 end
