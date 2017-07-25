c92e4d31f9f4790d28013d7be9cf1a7c6307bf75
SD Davis <davis@devcamp.com>
2017-07-25 15:29:44 +0000

    dev view tool init commit

---
+++ dev_view_tool/lib/dev_view_tool.rb
@@ -0,0 +1,7 @@
+require "dev_view_tool/version"
+require "dev_view_tool/renderer"
+
+
+module DevViewTool
+  # Your code goes here...
+end
---
+++ dev_view_tool/lib/dev_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DevViewTool
+  VERSION = "0.1.0"
+end
